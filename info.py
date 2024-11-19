67b6102593425ff347838f1b1dff28646731b569
shai1995 <76399884+shai1995@users.noreply.github.com>
2024-11-19 11:54:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,6 +31,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Mass')
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002359445972'))
 QR_CODE = environ.get('QR_CODE', 'https://envs.sh/0-Y.jpg')
+DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 #this vars is for when heroku or koyeb acc get banned, then change this vars as your file to link bot name
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002468523460'))
